fa3caffbfe77732e7e6351e5d86060918fa4ba76
Patrick Kwinten <patrick.kwinten@gmail.com>
2016-10-10 16:57:04 +0200

    Added node modules and build a boilertemplate

---
+++ ODP/WebContent/app/app.component.ts
@@ -0,0 +1,15 @@
+/*
+* 4. Component - app/app.component.ts
+*/
+
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'hello-world',
+	template: '<h1>Hello World - this is Angular 2!</h1>'
+})
+
+
+export class AppComponent {
+	// ... hier komt de logica
+}
---
+++ ODP/WebContent/app/main.ts
@@ -0,0 +1,8 @@
+// 5. Bootstrapper - app/main.ts
+
+import {bootstrap}    from 'angular2/platform/browser'
+
+// important to refer to './app' !!
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
